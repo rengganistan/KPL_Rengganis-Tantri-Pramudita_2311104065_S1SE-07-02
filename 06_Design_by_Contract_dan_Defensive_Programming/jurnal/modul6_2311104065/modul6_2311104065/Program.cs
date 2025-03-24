9f0fc2fcbdc046c6d509ad3ae0ea2b225dd7e936
rengganistan <rengganistantri4@gmail.com>
2025-03-25 02:46:40 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- 06_Design_by_Contract_dan_Defensive_Programming/jurnal/modul6_2311104065/modul6_2311104065/Program.cs
+++ 06_Design_by_Contract_dan_Defensive_Programming/jurnal/modul6_2311104065/modul6_2311104065/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul6_2311104065;
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeUser user = new SayaTubeUser("Rengganis");
+
+        string[] filmTitles = {
+            "Inception", "Interstellar", "The Dark Knight", "Parasite",
+            "Avengers: Endgame", "Forrest Gump", "The Shawshank Redemption",
+            "Titanic", "Spirited Away", "The Matrix"
+        };
+
+        foreach (string title in filmTitles)
+        {
+            SayaTubeVideo video = new SayaTubeVideo($"{title} oleh Rengganis");
+            user.AddVideo(video);
+        }
+
+        user.PrintAllVideoPlaycount();
+    }
+}
---
+++ 06_Design_by_Contract_dan_Defensive_Programming/jurnal/modul6_2311104065/modul6_2311104065/SayaTubeVideo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_2311104065
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            if (string.IsNullOrEmpty(title) || title.Length > 200)
+                throw new ArgumentException("Title cannot be empty or exceed 200 characters");
+
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count < 0 || count > 10000000)
+                throw new ArgumentOutOfRangeException("Count must be between 0 and 10,000,000");
+
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+
+        public int GetPlayCount()
+        {
+            return playCount;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+    }
+}
