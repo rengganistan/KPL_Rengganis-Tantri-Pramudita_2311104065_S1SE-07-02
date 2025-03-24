26697383d2556fe904b1bc4d448489f1597b7af8
rengganistan <168392672+rengganistan@users.noreply.github.com>
2025-03-24 23:44:08 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- 06_Design_by_Contract_dan_Defensive_Programming/jurnal/SayaTubeVideo.cs
+++ 06_Design_by_Contract_dan_Defensive_Programming/jurnal/SayaTubeVideo.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_2311104065
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"Video ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}\n");
+        }
+    }
+}
 
