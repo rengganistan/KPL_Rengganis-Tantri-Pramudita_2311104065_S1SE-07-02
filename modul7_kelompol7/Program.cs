b3cbb6e8b43b1948d6f9cff1aa3914478831b857
rengganis <rengganistantri4@gmail.com>
2025-04-24 01:08:24 +0700

    menambahkan kelas DataMahasiswa2311104065

---
+++ modul7_kelompol7/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using JurnalModul_7_Kelompok_7;
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("========== Deserialisasi Data Mahasiswa ==========");
+        var dataMahasiswa = new DataMahasiswa2311104065();
+        dataMahasiswa.ReadJSON();
+
+        Console.WriteLine("\n========== Deserialisasi Team Members ==========");
+        var teamMembers = new TeamMembers2311104065();
+        teamMembers.ReadJSON();
+
+        Console.WriteLine("\n========== Deserialisasi Glossary Item ==========");
+        var glossaryItem = new GlossaryItem2311104065();
+        glossaryItem.ReadJSON();
+        Console.ReadLine();
+    }
+}
