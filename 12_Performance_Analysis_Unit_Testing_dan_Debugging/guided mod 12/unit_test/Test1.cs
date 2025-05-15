1ac8a796e14f77e1a29873d8669ac2c0a6acdc3b
rengganis <rengganistantri4@gmail.com>
2025-05-15 20:20:17 +0700

    guided mod12

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/guided mod 12/unit_test/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace unit_test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestGrade()
+        {
+            String result1 = guided_mod12.Program.DetermineGrade(90);
+            Assert.AreEqual("A", result1);
+        }
+    }
+}
