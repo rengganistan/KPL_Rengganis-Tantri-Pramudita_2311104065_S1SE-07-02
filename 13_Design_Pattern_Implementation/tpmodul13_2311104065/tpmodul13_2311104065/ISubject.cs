d479c3869c7a30730b00987093659fdeacb6ba04
rengganis <rengganistantri4@gmail.com>
2025-05-20 23:10:40 +0700

    menambahkan tp mod 13

---
+++ 13_Design_Pattern_Implementation/tpmodul13_2311104065/tpmodul13_2311104065/ISubject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul13_2311104065
+{
+    public interface ISubject
+    {
+        // Attach an observer to the subject.
+        void Attach(IObserver observer);
+
+        // Detach an observer from the subject.
+        void Detach(IObserver observer);
+
+        // Notify all observers about an event.
+        void Notify();
+    }
+}
---
+++ 13_Design_Pattern_Implementation/tpmodul13_2311104065/tpmodul13_2311104065/Program.cs
@@ -0,0 +1,22 @@
+﻿using tpmodul13_2311104065;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // The client code.
+        var subject = new Subject();
+        var observerA = new ConcreteObserverA();
+        subject.Attach(observerA);
+
+        var observerB = new ConcreteObserverB();
+        subject.Attach(observerB);
+
+        subject.SomeBusinessLogic();
+        subject.SomeBusinessLogic();
+
+        subject.Detach(observerB);
+
+        subject.SomeBusinessLogic();
+    }
+}
