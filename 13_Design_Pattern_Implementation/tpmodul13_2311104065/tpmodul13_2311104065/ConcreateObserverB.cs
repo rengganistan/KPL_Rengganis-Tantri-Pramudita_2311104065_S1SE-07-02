821b5b6e02af76e885e958a7a9b49b304bc2312d
rengganis <rengganistantri4@gmail.com>
2025-05-27 22:19:41 +0700

    menambahkan tp modul 13

---
+++ 13_Design_Pattern_Implementation/tpmodul13_2311104065/tpmodul13_2311104065/ConcreateObserverB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tpmodul13_2311104065
+{
+    class ConcreteObserverB : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if (subject is Subject concreteSubject)
+            {
+                if (concreteSubject.State == 0 || concreteSubject.State >= 2)
+                {
+                    Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+                }
+            }
+        }
+    }
+}
---
+++ 13_Design_Pattern_Implementation/tpmodul13_2311104065/tpmodul13_2311104065/IObserver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul13_2311104065
+{
+    public interface IObserver
+    {
+        // Receive update from subject
+        void Update(ISubject subject);
+    }
+}
