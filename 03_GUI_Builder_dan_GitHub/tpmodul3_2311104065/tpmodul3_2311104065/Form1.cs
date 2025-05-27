af472e579ef61cdc984eed41b436671c2b56dbac
rengganis <rengganistantri4@gmail.com>
2025-05-27 21:48:08 +0700

    menambahkan tp modul 3

---
+++ 03_GUI_Builder_dan_GitHub/tpmodul3_2311104065/tpmodul3_2311104065/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace tpmodul3_2311104065
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputNama = textBox1.Text;
+
+            if (inputNama == "Rengganis Tantri") // Ganti dengan namamu
+            {
+                label1.Text = "Halo " + inputNama;
+            }
+            else
+            {
+                label1.Text = "Nama tidak cocok";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Boleh dikosongkan atau dihapus jika tidak dipakai
+        }
+    }
+}
