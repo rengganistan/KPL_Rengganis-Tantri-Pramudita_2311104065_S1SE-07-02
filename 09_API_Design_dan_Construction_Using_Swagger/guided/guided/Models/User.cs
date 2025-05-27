761ab50923bd968273b39b6b1865fca3e332490e
rengganis <rengganistantri4@gmail.com>
2025-05-27 22:05:01 +0700

    menambahkan guided modul 9

---
+++ 09_API_Design_dan_Construction_Using_Swagger/guided/guided/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace guided.models
+{
+    public class user
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
