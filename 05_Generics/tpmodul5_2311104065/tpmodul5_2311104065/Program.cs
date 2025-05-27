0ec54f02c1e75010c85b5a289c5e508b59183f19
rengganis <rengganistantri4@gmail.com>
2025-05-27 21:53:51 +0700

    menambahkan modul 5

---
+++ 05_Generics/tpmodul5_2311104065/tpmodul5_2311104065/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Dian");
+
+        DataGeneric<string> data = new DataGeneric<string>("2311104065");
+        data.PrintData();
+    }
+}
