0d11fe81178674f4af314d9fdfa96343ff7b064c
rengganis <rengganistantri4@gmail.com>
2025-05-27 22:05:40 +0700

    menambahkan jurnal modul 9

---
+++ 09_API_Design_dan_Construction_Using_Swagger/modul9_2311104065/modul9_2311104065/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace modul9_2311104065.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie()
+        {
+            Stars = new List<string>();
+        }
+    }
+}
