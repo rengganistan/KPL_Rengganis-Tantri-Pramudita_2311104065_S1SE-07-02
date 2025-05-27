e0111b2fa1b763b18c10f736b751eea9cfdb646a
rengganis <rengganistantri4@gmail.com>
2025-05-27 22:04:15 +0700

    memperbaiki

--- 09_API_Design_dan_Construction_Using_Swagger/guided/guided/Models/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace guided.Models
-{
-    public class UserDto
-    {
-        public string name { get; set; }
-        public string email { get; set; }
-    }
-}
--- 09_API_Design_dan_Construction_Using_Swagger/tpmodul9_2311104065/tpmodul9_2311104065/Mahasiswa.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace tpmodul9_2311104065.Models
-{
-    public class Mahasiswa
-    {
-
-        public string Nama { get; set; }
-        public string Nim { get; set; }
-    }
-}
